cff80870e49e761b39b699cdb33ad86bd5c051d7
lixianmin <lixianmin@live.cn>
2026-03-17 12:18:31 +0800

    chore: add restart.sh for build and restart

---
+++ restart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Stopping old pc process..."
+./pc gateway stop
+sleep 1
+
+echo "Building pc..."
+make build
+
+echo "Building plugins..."
+make build-plugins
+
+echo "Starting pc..."
+./pc gateway start
