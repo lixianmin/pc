49d7a7564a3a2ee79d7c520280f8a348086dee28
lixianmin <lixianmin@live.cn>
2026-03-24 12:23:08 +0800

    feat(gateway): enable BAML for LLM calls

--- internal/gateway/daemon.go
+++ internal/gateway/daemon.go
@@ -201,6 +201,9 @@ func (my *Daemon) Run() error {
 
 	eng := engine.NewEngine(pm)
 
+	// TEMP: 启用 BAML，阶段 3 删除此行
+	eng.SetUseBAML(true)
+
 	if cfg.GetPromptsDir() != "" {
 		recorder := debug.NewPromptRecorder(cfg.GetPromptsDir(), cfg.GetMaxPromptFiles())
 		eng.SetPromptRecorder(recorder)
